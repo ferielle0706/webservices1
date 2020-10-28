718f93d24a83fae3c76e823585e411d625313add
ferielle0706 <ferielle.rhl@gmail.com>
2020-10-28 20:45:28 +0100

    first commit

---
+++ client.php
@@ -0,0 +1,16 @@
+<?php
+require_once('lib/nusoap.php');
+$wsdl = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL";
+
+
+$client = new nusoap_client($wsdl, 'wsdl');
+$err = $client->getError();
+if ($err) {
+   echo '<h2>L\'erreur est :</h2>' . $err;
+   exit();
+}
+
+$result=$client->call('CapitalCity', array('sCountryISOCode'=>'US'));
+
+echo 'The Capital City of UNITED STATES is <strong>'.($result['CapitalCityResult']).'</strong><br>';
+?>
